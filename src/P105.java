fa59d0ca6a1172aa7bf74747d9f117038159f550
venturas55 <venturas55@hotmail.com>
2024-04-28 14:08:01 +0200

    readme retoque

--- src/P105.java
+++ src/P105.java
@@ -1,20 +1,75 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package p105;
 
-/**
- *
- * @author VeNtU
- */
-public class P105 {
+import java.util.Locale;
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+public class P105 {
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner in = new Scanner(System.in).useLocale(Locale.US);
+        float venta;
+        float[] semana = new float[6];
+        String[] diaSemana = { "MARTES", "MIERCOLES", "JUEVES", "VIERNES", "SABADO", "DOMINGO","EMPATE" };
+        String textoFinal = "";
+        // diaSemana=["LUNES","MARTES","MIERCOLES","JUEVES","VIERNES","SABADO","DOMINGO"];
+        int i = 0;
+        do {
+            venta = in.nextFloat();
+            //System.out.println(">"+venta);
+            if (venta == -1) {
+                break;
+            }
+
+            semana[i] = venta;
+            i++;
+            if (i == 6) {
+                if (semana[5] > media(semana))
+                    textoFinal = diaSemana[maximo(semana)] + " " + diaSemana[minimo(semana)] + " SI";
+                else
+                    textoFinal = diaSemana[maximo(semana)] + " " + diaSemana[minimo(semana)] + " NO";
+                System.out.println(textoFinal);
+                i = 0;
+                textoFinal = "";
+            }
+
+        } while (true);
+        in.close();
+
     }
-    
+
+    public static int maximo(float[] array) {
+        float maximo = array[0];
+        int index=0;
+        for (int i = 1; i < array.length - 1; i++) {
+            if (array[i] > maximo) {
+                maximo = array[i];
+                index=i;
+            }else if(array[i] == maximo) {
+                index=6;
+            }
+        }
+        return index;
+    }
+
+    public static int minimo(float[] array) {
+        float minimo = array[0];
+        int index=0;
+        for (int i = 1; i < array.length - 1; i++) {
+            if (array[i] < minimo) {
+                minimo = array[i];
+                index=i;
+            }else if(array[i] == minimo) {
+                index=6;
+            }
+        }
+        return index;
+    }
+
+    public static float media(float[] array) {
+        float suma = 0;
+        for (int i = 0; i < array.length - 1; i++) {
+            suma += array[i];
+        }
+        return (suma / array.length);
+    }
+
 }
